046bf799b00e65cf33028de80ae3ee080c9420c2
Chris Lam <chris.lam@asknice.ly>
2021-03-18 11:12:19 +1300

    Add type alias and interface examples, add questions slide

---
+++ slides/09.ts
@@ -0,0 +1,8 @@
+// A simple type alias of a primitive
+type Description = string;
+
+type Vehicle = {
+  // What properties does a car have?
+};
+
+// Vehicles are more than just cars...
---
+++ slides/11.ts
@@ -0,0 +1,11 @@
+interface Animal {
+  numberOfLegs: number;
+  hasFur: boolean;
+}
+
+const bear: Animal = {
+  numberOfLegs: 4,
+  hasFur: true
+};
+
+// Some animals have horns...
