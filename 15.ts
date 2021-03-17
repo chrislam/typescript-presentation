26271faab35b519a6d2dabee1f4ad40f6dc471e3
Chris Lam <chris.lam@asknice.ly>
2021-03-18 10:47:24 +1300

    Add example of something a bit inelegant about enums

---
+++ 15.ts
@@ -0,0 +1,10 @@
+// Here's some values
+enum Status {
+  Gold = "gold",
+  Silver = "silver",
+  Bronze = "bronze",
+}
+
+// What if we want them in a specific order?
+
+// We need to put them in an array
